88e9c02b98a0a339520c9ad0dbeb6df6c373595d
greed <995762937@qq.com>
2023-07-04 09:52:52 +0800

    pool_token only

--- pk_generator.py
+++ pk_generator.py
@@ -13,11 +13,16 @@ def run():
     expires_in = 0
     unique_name = getenv('UNIQUE_NAME', 'my share token')
     pool_token = getenv('POOL_TOKEN', '')
+    only_pk = getenv('ONLY_PK', False)
     current_dir = path.dirname(path.abspath(__file__))
     credentials_file = path.join(current_dir, 'file', 'credentials.txt')
     tokens_file = path.join(current_dir, 'file', 'tokens.txt')
     share_tokens_file = path.join(current_dir, 'file', 'share_tokens.txt')
-
+    if only_pk:
+        with open(share_tokens_file, 'r', encoding='utf-8') as f:
+            share_tokens = f.read().split('\n')
+        pool_token_gen(share_tokens, pool_token)
+        return
     with open(credentials_file, 'r', encoding='utf-8') as f:
         credentials = f.read().split('\n')
     credentials = [credential.split(',', 1) for credential in credentials]
@@ -38,16 +43,20 @@ def run():
             'share_token': 'None',
         }
         token_keys.append(token_info)
-
-        try:
-            token_info['token'] = Auth0(username, password, proxy).auth(True)
-            print('Login success: {}, {}'.format(username, progress))
-        except Exception as e:
-            err_str = str(e).replace('\n', '').replace('\r', '').strip()
-            print('Login failed: {}, {}'.format(username, err_str))
-            token_info['token'] = err_str
+        is_fail = False
+        for i in range(0, 3):
+            try:
+                token_info['token'] = Auth0(username, password, proxy).auth(True)
+                print('Login success: {}, {}'.format(username, progress))
+                break
+            except Exception as e:
+                err_str = str(e).replace('\n', '').replace('\r', '').strip()
+                print('Login failed: {}, {}'.format(username, err_str))
+                token_info['token'] = err_str
+                if i == 2:
+                    is_fail = True
+        if is_fail:
             continue
-
         data = {
             'unique_name': unique_name,
             'access_token': token_info['token'],
@@ -71,20 +80,23 @@ def run():
         for token_info in token_keys:
             f.write('{}\n'.format(token_info['share_token']))
 
-    if count > 20:
+    if count > 100:
         print('too many accounts!')
         return
+    pool_token_gen([token_info['share_token'] for token_info in token_keys], pool_token)
 
+
+def pool_token_gen(share_tokens: list, pool_token: str = ''):
     data = {
-        'share_tokens': '\n'.join([token_info['share_token'] for token_info in token_keys]),
+        'share_tokens': '\n'.join(share_tokens),
         'pool_token': pool_token
     }
     resp = requests.post('https://ai.fakeopen.com/pool/update', data=data)
 
     if resp.status_code == 200:
-        print('pool token: {}', resp.json()['pool_token'])
+        print('pool token: ', resp.json()['pool_token'])
     else:
-        print('generate pool token failed: {}'.format(resp.text))
+        print('generate pool token failed: '.format(resp.text))
 
 
 if __name__ == '__main__':
